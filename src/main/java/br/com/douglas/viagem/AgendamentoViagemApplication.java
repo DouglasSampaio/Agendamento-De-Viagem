4ae8f1660fe4c03908b6a44f962f17d6bf482bb4
Douglas Araujo <douglasaraujosampaio@gmail.com>
2023-07-14 15:40:18 -0300

    first commit

---
+++ src/main/java/br/com/douglas/viagem/AgendamentoViagemApplication.java
@@ -0,0 +1,13 @@
+package br.com.douglas.viagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoViagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoViagemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/douglas/viagem/model/User.java
@@ -0,0 +1,10 @@
+package br.com.douglas.viagem.model;
+
+
+public class User {
+
+	private String nome;
+	private String email;
+	private String senha;
+	
+}
